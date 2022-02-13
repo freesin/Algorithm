543a11b396a1a3eef83b2e03c7341acfe9e4fb0a
parkdohyun <freesin94@gmail.com>
2022-02-13 19:08:52 +0900

    [프로그래머스] 정렬 문제

---
+++ programmers/sort.py
@@ -0,0 +1,13 @@
+#[1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
+
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+array = [1, 5, 2, 6, 3, 7, 4]
+print(''.join(str(e) for e in array))
+
+def solution(array, commands):
+    a = ''
+    for i in array:
+        a += str(i)
+    return a
+
+print(solution(array, commands))
