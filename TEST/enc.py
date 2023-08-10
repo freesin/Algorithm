4476a630e4db620825ec57a66f0d216dbf26f4d6
parkdohyun <freesin94@gmail.com>
2023-08-11 01:33:45 +0900

    test1

---
+++ TEST/enc.py
@@ -0,0 +1,84 @@
+#-*- coding: utf-8 -*-
+# 파일 암복호화 해제후 다운로드까지 하기
+import os, struct
+from Crypto.Cipher import AES
+
+FILE_KEY = "c11007050f4b4740b30155547a95834c"
+
+class CryptHelper_file:
+    def __init__(self):
+        self.key = FILE_KEY
+
+    def decrypt_file(self, in_filename, out_filename=None, chunksize=24 * 1024):
+        """ Decrypts a file using AES (CBC mode) with the
+            given key. Parameters are similar to encrypt_file,
+            with one difference: out_filename, if not supplied
+            will be in_filename without its last extension
+            (i.e. if in_filename is 'aaa.zip.enc' then
+            out_filename will be 'aaa.zip')
+        """
+        if not out_filename:
+            out_filename = os.path.splitext(in_filename)[0]
+
+        with open(in_filename, 'rb') as infile:
+            origsize = struct.unpack('<Q', infile.read(struct.calcsize('Q')))[0]
+            iv = infile.read(16)
+            decryptor = AES.new(self.key, AES.MODE_CBC, iv)
+
+            with open(out_filename, 'wb') as outfile:
+                while True:
+                    chunk = infile.read(chunksize)
+                    if len(chunk) == 0:
+                        break
+                    outfile.write(decryptor.decrypt(chunk))
+
+                outfile.truncate(origsize)
+
+    def encrypt_file(self, in_filename, out_filename=None, chunksize=64 * 1024):
+        """ Encrypts a file using AES (CBC mode) with the
+            given key.
+
+            key:
+                The encryption key - a string that must be
+                either 16, 24 or 32 bytes long. Longer keys
+                are more secure.
+
+            in_filename:
+                Name of the input file
+
+            out_filename:
+                If None, '<in_filename>.enc' will be used.
+
+            chunksize:
+                Sets the size of the chunk which the function
+                uses to read and encrypt the file. Larger chunk
+                sizes can be faster for some files and machines.
+                chunksize must be divisible by 16.
+        """
+        if not out_filename:
+            out_filename = in_filename + '.enc'
+
+        iv = os.urandom(16)
+        encryptor = AES.new(self.key, AES.MODE_CBC, iv)
+        filesize = os.path.getsize(in_filename)
+
+        with open(in_filename, 'rb') as infile:
+            with open(out_filename, 'wb') as outfile:
+                outfile.write(struct.pack('<Q', filesize))
+                outfile.write(iv)
+
+                while True:
+                    chunk = infile.read(chunksize)
+                    if len(chunk) == 0:
+                        break
+                    elif len(chunk) % 16 != 0:
+                        chunk += b' ' * (16 - len(chunk) % 16)
+
+                    outfile.write(encryptor.encrypt(chunk))
+
+
+
+file_name = '/Users/parkdohyun/Downloads/20230105100001_EWXML_1.zip.enc'
+
+ch = CryptHelper_file()
+ch.decrypt_file(file_name)
